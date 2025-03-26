f58cc32acff73ea83861cb9715ed763b302153db
Aryankr484 <Aryankr484>
2025-03-26 10:36:36 +0530

    made some changes

--- app.js
+++ app.js
@@ -101,16 +101,16 @@ app.get('/profile', isLoggedIn, async (req, res) => {
     res.render("profile", { user });
 });
 
-app.get('/like/:id', isLoggedIn, async (req, res) => {
-    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
-    if (post.likes.indexOf(req.user.userid) == -1) {
-        post.likes.push(req.user.userid);
-    } else {
-        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
-    }
-    await post.save();
-    res.redirect("/profile");
-});
+// app.get('/like/:id', isLoggedIn, async (req, res) => {
+//     let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+//     if (post.likes.indexOf(req.user.userid) == -1) {
+//         post.likes.push(req.user.userid);
+//     } else {
+//         post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+//     }
+//     await post.save();
+//     res.redirect("/profile");
+// });
 
 app.get('/edit/:id', isLoggedIn, async (req, res) => {
     let post = await postModel.findOne({ _id: req.params.id }).populate("user");
